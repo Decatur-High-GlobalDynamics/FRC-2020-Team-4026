51f2985f5c8cf0160b974d1a1e5da3eca662062a
Zach Slaton <knxtdoor@aol.com>
2020-02-22 17:52:03 -0500

    Add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -63,5 +63,9 @@ public final class Constants {
 
     // DIO Ports
     public static final int TurretLimitDIO = 0;
+    public static final int IndexerLowerLimitDIO = 1;
+    public static final int IndexerMiddleLimitDIO = 2;
+    public static final int IndexerUpperLimitDIO = 3;
+
 
 }
