8264a75b2ae6b0230ccce936f7c7d32a4cc9f110
unknown <keonsanavandijoon@gmail.com>
2020-03-03 21:01:35 -0500

    Add command to point straight ahead

---
+++ src/main/java/frc/robot/commands/PointTurretStraightAhead.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Utils;
+import frc.robot.subsystems.TurretSubsystem;
+
+public class PointTurretStraightAhead extends CommandBase {
+  TurretSubsystem turret;
+  /**
+   * Creates a new PointTurretStraightAhead.
+   */
+  public PointTurretStraightAhead(TurretSubsystem turret) {
+    this.turret = turret;
+    addRequirements(turret);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    turret.startRotatingToPosition(Math.PI/2);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    turret.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return Utils.checkTolerance(Math.PI/2, turret.getRadians(), 1/(36*Math.PI));
+  }
+}
