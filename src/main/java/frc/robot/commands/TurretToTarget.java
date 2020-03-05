1f293687ebcac9732878736c7913f183746b145d
ABoredIdiot <80bertbeel@csdecatur.net>
2020-03-04 19:50:05 -0500

    First attempt at TurretToVision
    Changed
    - Logic that should make TurretToTarget work

--- src/main/java/frc/robot/commands/TurretToTarget.java
+++ src/main/java/frc/robot/commands/TurretToTarget.java
@@ -8,13 +8,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TurretSubsystem;
 
 public class TurretToTarget extends CommandBase {
   /**
    * Creates a new TurretToTarget.
    */
-  public TurretToTarget() {
+  private final TurretSubsystem turret;
+
+  public TurretToTarget(TurretSubsystem turret) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.turret = turret;
+    addRequirements(turret);
   }
 
   // Called when the command is initially scheduled.
@@ -25,16 +30,22 @@ public class TurretToTarget extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    if (turret.getVisionXAngle() == 4026){
+      this.end(true);
+    } else {
+      turret.startRotatingToEncoderPosition((long) (turret.getTicks() + (-50 * Math.signum(turret.getVisionXAngle()))));
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    turret.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return turret.getVisionXAngle() == 4026 || turret.getVisionXAngle() <= 0.5;
   }
 }
