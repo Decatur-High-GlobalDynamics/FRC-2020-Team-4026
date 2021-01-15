cdbf5f17e3830c5532e82df6df409a2f9538304a
Garrett Goebel <garrett.goebel@gmail.com>
2021-01-15 09:52:23 -0500

    eliminate warnings: remove unused imports, values, etc.

--- src/main/java/frc/robot/commands/drivingCommands/DriveEncoders.java
+++ src/main/java/frc/robot/commands/drivingCommands/DriveEncoders.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.Constants;
-import frc.robot.TeamUtils;
 
 public class DriveEncoders extends CommandBase {
 
