6b75d1810325d1da51463276d2a68981591604e7
Keon <keonsanavandijoon@gmail.com>
2021-01-28 15:46:10 -0500

    Fixed some comments to be clearer

--- src/main/java/frc/robot/commands/drivingCommands/GTADriveCommand.java
+++ src/main/java/frc/robot/commands/drivingCommands/GTADriveCommand.java
@@ -36,15 +36,15 @@ public class GTADriveCommand extends CommandBase {
     //If both triggers are pressed, don't move as they shouldn't be
     if (leftTrigger.getAsBoolean() && rightTrigger.getAsBoolean()) {
       powerToSet = 0;
-      //If just the left one is pressed, go backwards aat a speed based on the right stick
+      //If just the left one is pressed, go backwards at a speed based on the right stick
     } else if (leftTrigger.getAsBoolean()) {
       //If the right stick is all the way forward, go at max, if it's not touched, go at half, if it's pulled back, don't move
       powerToSet = -((rightStick.getAsDouble() + 1) / 2);
-      //If just the right trigger is pressed, go forward
+      //If just the right trigger is pressed, go forward based on right stick
     } else if (rightTrigger.getAsBoolean()) {
       powerToSet = (rightStick.getAsDouble() + 1) / 2;
     }
-    //This feeds the powers to curve drive - the power, the turn, which is leftStick's value, and whether power is super low - if power is super low we assume we're turning in place
+    //This feeds the powers to curve drive - the power, the turn, which is leftStick's value, and whether power is below a threshold - if it is we assume we're turning in place
     driveTrain.curveDrive(powerToSet, leftStick.getAsDouble(), Math.abs(powerToSet) < 0.02);
   }
 }
