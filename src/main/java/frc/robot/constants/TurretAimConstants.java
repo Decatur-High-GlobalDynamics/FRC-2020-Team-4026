14a0da2047ef685b9fe6218f424e6d05460bba93
Keon <keonsanavandijoon@gmail.com>
2021-03-27 14:14:53 -0400

    Add a file that didn't get committed

---
+++ src/main/java/frc/robot/constants/TurretAimConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class TurretAimConstants {
+    public static final double heightDifferenceForVision = 0;
+
+    public static final double limelightAngle = 0;
+
+    public static final double limelightRightDist = 0;
+}
