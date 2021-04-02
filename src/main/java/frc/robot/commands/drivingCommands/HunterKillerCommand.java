ac6843d1beaeefce192decbbec11fd9e18525026
Hayden Carpenter <haydencarp@outlook.com>
2021-04-02 11:19:55 -0400

    Update HunterKillerCommand.java

--- src/main/java/frc/robot/commands/drivingCommands/HunterKillerCommand.java
+++ src/main/java/frc/robot/commands/drivingCommands/HunterKillerCommand.java
@@ -88,13 +88,16 @@ public class HunterKillerCommand extends CommandBase {
           this.alignCounter++;
 
         } else {
-
-          desiredSpeed = angleOff / (this.alignCounter * 2);
-          // Decrease speed as target locks on.
-          // Desired Speed is set to the angle that the rotation is currently off divided by the
-          // number of
-          // alignment frames to prevent infinite loop in case we raise requiredFramesForTargetLock
-          // (which we probably will)
+          if (this.alignCounter > 0) {
+            desiredSpeed = angleOff / (this.alignCounter * 2);
+            // Decrease speed as target locks on.
+            // Desired Speed is set to the angle that the rotation is currently off divided by the
+            // number of alignment frames to prevent infinite loop in case we raise requiredFramesForTargetLock
+            // (which we probably will)
+          }
+          else {
+            desiredSpeed = angleOff;
+          }
 
           if (ballX > angleMid) {
 
