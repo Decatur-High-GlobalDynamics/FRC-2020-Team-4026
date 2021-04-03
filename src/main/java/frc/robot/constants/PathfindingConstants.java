0bbafafdcf9768b4a291039d9fb0db8cfc3068ab
Keon <keonsanavandijoon@gmail.com>
2021-04-03 14:43:08 -0400

    Build paths + set constants

--- src/main/java/frc/robot/constants/PathfindingConstants.java
+++ src/main/java/frc/robot/constants/PathfindingConstants.java
@@ -4,11 +4,11 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 
 public class PathfindingConstants {
   // These are TBD: They will be used as feedforward for pathfinding
-  public static final double ks = 0;
-  public static final double kv = 0;
-  public static final double ka = 0;
+  public static final double ks = 0.669;
+  public static final double kv = 2.3;
+  public static final double ka = 0.212;
   // This is TBD: Will be a PID constant for pathfinding
-  public static final double kPDriveVel = 0;
+  public static final double kPDriveVel = 2.25;
   // This is the width between our left and right wheels
   public static final double kTrackWidthMeters = 0.581025;
   // This uses that width to convert from chassis directions (power and angle) to powers for left
