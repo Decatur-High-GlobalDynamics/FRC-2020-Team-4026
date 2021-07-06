c0c8375b21193dff26b1db9a3f99d0b2591d8178
Keon <keonsanavandijoon@gmail.com>
2021-07-06 16:51:09 -0400

    Add stopping

--- src/main/java/frc/robot/commands/shooterCommands/AutoShootPower.java
+++ src/main/java/frc/robot/commands/shooterCommands/AutoShootPower.java
@@ -30,7 +30,9 @@ public class AutoShootPower extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    shooter.stop();
+  }
 
   // Returns true when the command should end.
   @Override
