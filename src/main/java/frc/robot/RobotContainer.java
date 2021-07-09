a09efc115bfe4fdc9ceff183c4f3edd670ee4cf0
Keon <keonsanavandijoon@gmail.com>
2021-07-08 22:17:09 -0400

    Add comment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -296,7 +296,7 @@ public class RobotContainer {
     // This indexes the vertical indexer up
     Command vertUp = new VerticalIndexerUpCommand(verticalIndexer);
 
-    //This lets the shooter spin, then keeps it spinning and indexes up for 5 seconds to allow all balls to be shot, then drives forwards. Times can be adjusted as needed
+    //This lets the shooter spin, then keeps it spinning and indexes up for 5 seconds to allow all balls to be shot, then drives forwards. While it does this the turret aims. Times can be adjusted as needed
     return aimTurret.alongWith(spinUp.andThen((shoot.alongWith(horizIn.alongWith(vertUp)).withTimeout(5)).andThen(driveForward)));
   }
 
